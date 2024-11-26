ae640e4cb2458b04d517c5d19fc3b5dfe9fd9a7b
Ivan Sakharov <vingving648@gmail.com>
2024-11-26 20:26:32 +0300

    init commit

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,9 +1,21 @@
 from . import *
+import ollama
 
 router = Router()
 
 
 @router.message(Command('start'))
 async def start(message: types.Message, state: FSMContext):
-    await message.answer('Hello!')
- 
+    await message.answer('Write your request:')
+
+
+@router.message(F.text)
+async def ask_llm(message: types.Message):
+    response = ollama.chat(model='llama3.2:1b', messages=[
+        {
+            'role': 'user',
+            'content': message.text,
+        },
+    ])
+    response_text = response['message']['content']
+    await message.answer(text=f'🤖: {response_text}')
